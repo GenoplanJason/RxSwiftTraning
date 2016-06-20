76188c355367e585d414ff2ef4e251c41d5c1ba6
Jason <jason@genoplan.com>
2016-06-20 10:57:31 +0900

    ObjectiveCPattern

---
+++ PatternObjC/ObjectivePattern/ObjectivePattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectivePattern
+//
+//  Created by Jason on 2016. 6. 20..
+//  Copyright © 2016년 genoplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PatternObjC/ObjectivePattern/ObjectivePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectivePattern
+//
+//  Created by Jason on 2016. 6. 20..
+//  Copyright © 2016년 genoplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
